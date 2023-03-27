bc829a967d92217375117ddc3e181a0e60496eae
hencezze <104189173+hencezze@users.noreply.github.com>
2023-03-28 00:33:36 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 # Import re module
 import re
+import sys
 
 # Take any string data
-string = input("Enter a string value: ")
-# Define the searching pattern
 PATTERN = '^[A-Z]'
 
 # match the pattern with input value
-found = re.match(PATTERN, string)
+found = re.match(PATTERN, sys.argv[0])
 
 # Print message based on the return value
 if found:
