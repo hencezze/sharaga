7069b130eb385893fc3ce2ddcf6247956d4bf8b4
hencezze <104189173+hencezze@users.noreply.github.com>
2023-03-28 00:50:49 +0300

    Basota

--- main.py
+++ main.py
@@ -1,14 +1,2 @@
-import re
-import sys
-
-# Take any string data
-PATTERN = '^[A-Z]'
-word=input()
-# match the pattern with input value
-found = re.match(PATTERN,word )
-
-# Print message based on the return value
-if found:
-    print("DA")
-else:
-    print("NET")
+txt = input("Введите что-нибудь, чтобы проверить это: ")
+print("Это то, что вы только что ввели?", txt)
