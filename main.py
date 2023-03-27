98ce6ca327a85f9ec67e949be2588ba647925aed
hencezze <104189173+hencezze@users.noreply.github.com>
2023-03-27 18:44:37 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print("2")
+# Initialize counter
+counter = 1
+# Iterate the loop 5 times
+while counter < 6:
+    # Print the counter value
+    print("The current counter value: %d" % counter)
+    # Increment the counter
+    counter = counter + 1
